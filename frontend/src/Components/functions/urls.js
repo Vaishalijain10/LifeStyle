d53381b8a3b815f1a7b14890a394047cc80b35d2
Vaishalijain10 <vaishalijain.vj.10@gmail.com>
2024-12-04 22:15:53 +0530

    frontend deployment setup

--- frontend/src/Components/functions/urls.js
+++ frontend/src/Components/functions/urls.js
@@ -1,5 +1,12 @@
-// backend running url
-export const baseUrl = `http://localhost:1008`;
+
+
+// Dynamically set the base URL based on the environment
+const isLocalhost = window.location.hostname === "localhost";
+
+export const baseUrl = isLocalhost
+  ? "http://localhost:1008" // Local backend
+  : process.env.REACT_APP_BASE_URL; // Deployed backend
+
 
 // routes from app.js
 export const userUrl = `${baseUrl}/users`;
